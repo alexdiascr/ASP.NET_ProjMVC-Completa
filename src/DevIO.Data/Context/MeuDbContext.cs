b4d26237a3523671df7f09cbe8aa98dd976a3777
alexdiascr <git config --global user.email eng.alexdiasc@gmail.comgit config --global user.email>
2023-01-05 13:29:03 -0300

    Finalizando curso

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,37 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Evitar, caso haja esquecimento de mapear algo, que um campo vá com um 
+            //nvachar(max)
+            //Abaixo, escreve-se uma garantia, caso um campo(tipo string)
+            //não tenha configuração de tamanho, tenha um tamanho mínimo
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            //Configurando para registrar cada mapping criado, de uma vez só, é feito via reflection
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(MeuDbContext).Assembly);
+
+            //Impendindo que uma classe que está sendo representando em uma tabela do banco ao ser excluido
+            //um item da mesma 
+            //evite de levar os filhos juntos
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+                relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/DevIO.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,26 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository 
+    {
+        public FornecedorRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Fornecedor>? ObterFornecedorEndereco(Guid Id)
+        {
+            return await Db?.Fornecedores.AsNoTracking().Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == Id);
+        }
+
+        public async Task<Fornecedor>? ObterFornecedorProdutosEndereco(Guid Id)
+        {
+            return await Db?.Fornecedores.AsNoTracking()
+                .Include(c => c.Produtos)
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == Id);
+        }
+    }
+}
